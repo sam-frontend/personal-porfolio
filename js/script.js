9bbc9f2f9488d06e15b831c1665b0b63e7c4a232
Sam ugonna <sam_ug@yahoo.com>
2022-10-30 20:10:05 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,9 +32,4 @@ document.querySelectorAll('a').forEach(links =>{
         cursor1.classList.remove('active');
         cursor2.classList.remove('active');
     }
-    function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
-
-});
+    
