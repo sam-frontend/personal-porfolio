172e1704e33a165fd9b85ed131f573db477056dd
Sam ugonna <sam_ug@yahoo.com>
2023-01-22 20:49:57 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
+    menu.classList.toggle('fas fa-times');
     header.classList.toggle('active');
 }
 
