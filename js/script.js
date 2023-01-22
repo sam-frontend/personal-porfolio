cddb5dd71e9f6833745f6a8c1b92a7e6348fba3b
Sam ugonna <sam_ug@yahoo.com>
2023-01-22 20:07:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ let menu = document.querySelector('#menu-bars');
 let header = document.querySelector('header');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
+    menu.classList.toggle('fas fa-times');
     header.classList.toggle('active');
 }
 
 window.onscroll = () =>{
-    menu.classList.remove('fa-times');
+    menu.classList.remove('fas fa-times');
     header.classList.remove('active');
 }
 
